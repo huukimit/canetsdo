be862f274430834c6bb8bb73ede5c6a0ff7c7c8c
Thanh <chithanh1012@gmail.com>
2017-03-25 16:34:36 +0700

    Chat bot

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -296,5 +296,14 @@
     <script type="text/javascript" src="{{ asset('public/frontend/js/owl.carousel.js') }}"></script>
     <script type="text/javascript" src="{{ asset('public/frontend/js/myscript.js') }}"></script>
     <!-- <script type="text/javascript" src="{{ asset('public/frontend/js/my.js') }}"></script> -->
+    <!--Start of Zendesk Chat Script-->
+    <script type="text/javascript">
+    window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+    d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+    $.src="https://v2.zopim.com/?2LDrsMcrCev0bP2ift5CtSHYhMbslW6Q";z.t=+new Date;$.
+    type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+    </script>
+    <!--End of Zendesk Chat Script-->
 </body>
 </html>
