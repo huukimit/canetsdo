e31fd713587a260415be30771455899e336744f5
Thanh <chithanh1012@gmail.com>
2017-03-26 17:17:09 +0700

    comment sound 1

--- app/Models/Notify/Notify.php
+++ app/Models/Notify/Notify.php
@@ -42,7 +42,7 @@ class Notify extends BaseModel {
      * @param type $sound
      * @return Push notify for ios
      */
-    static function Push2Ios($deviceToken = "", $message = "", $push_data = array(), /*$badge = -1, $sound = 'default',*/ $app = 'laodong') {
+    static function Push2Ios($deviceToken = "", $message = "", $push_data = array(), $app = 'laodong' , $badge = -1, $sound = 'default') {
         if (!$deviceToken || !$message) {
             return array("status" => -1, "message" => "No data", "data" => array());
         }
