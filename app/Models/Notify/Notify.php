807b62dd695a355d7af136085d11ffc7c0e88415
Thanh <chithanh1012@gmail.com>
2017-04-22 11:40:39 +0700

    22/04 Update

--- app/Models/Notify/Notify.php
+++ app/Models/Notify/Notify.php
@@ -77,7 +77,6 @@ class Notify extends BaseModel {
             $filePem = Config::get('services.device.ios.pem_file_dir');
             $passwordPem = Config::get('services.device.ios.pem_pass');
         }
-        Log::info(['ios_server' => $ios_server, 'filePem' => $filePem, 'passwordPem' => $passwordPem]);
 
         stream_context_set_option($ctx, 'ssl', 'local_cert', $filePem);
         stream_context_set_option($ctx, 'ssl', 'passphrase', $passwordPem);
--- resources/views/admin/labor_edit.blade.php
+++ resources/views/admin/labor_edit.blade.php
@@ -6,7 +6,16 @@
         <div class="panel panel-default">
             <div class="panel-heading">Detail laborer</div>
             <div class="panel-body">
-            	<form action="" class="form-group">
+            	<form action=""  role="form">
+				<div class="col-md-6">
+					<div class="form-group">
+						<label>Text Input</label>
+						<input class="form-control" placeholder="Placeholder">
+					</div>
+				</div>
+				<div class="col-md-6">
+					
+				</div>
             		
             	</form>
             </div>
