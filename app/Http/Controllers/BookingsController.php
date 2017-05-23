d876ae91b0a6b8da917829c284b45bbf9fc40426
Thanh <chithanh1012@gmail.com>
2017-05-23 23:25:17 +0700

    23/05

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -36,7 +36,7 @@ class BookingsController extends Controller {
 	                'device_token' => $customer->device_token,
 	            ];
 	            if ($i == 10) {
-	                // Log::info(['count' => $eachGroup]);
+	                Log::info(['count' => $eachGroup]);
 	                Queue::later(5, new PushNotifyToDevices($eachGroup, 'GV1L' . $post['address'], $pushData, $booking_id));
 	                $i = 0;
 	                $eachGroup = [];
